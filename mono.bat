9bc6524168349e2f89009348f473e78c9b0ff81c
Ksfour <78544646+Ks4four@users.noreply.github.com>
2023-10-13 03:18:38 +0800

    Add files via upload

---
+++ mono.bat
@@ -0,0 +1,13 @@
+cd D:\font-spoofing\ttf
+ttx -m D:\font-spoofing\input\sarasa-mono-sc-regular.ttf -o .\msyh0.ttf D:\font-spoofing\Original\ttc0\msyh.ttx
+ttx -m D:\font-spoofing\input\sarasa-mono-sc-regular.ttf -o .\msyh1.ttf D:\font-spoofing\Original\ttc1\msyh.ttx
+ttx -m D:\font-spoofing\input\sarasa-mono-sc-bold.ttf -o .\msyhbd0.ttf D:\font-spoofing\Original\ttc0\msyhbd.ttx
+ttx -m D:\font-spoofing\input\sarasa-mono-sc-bold.ttf -o .\msyhbd1.ttf D:\font-spoofing\Original\ttc1\msyhbd.ttx
+ttx -m D:\font-spoofing\input\sarasa-mono-sc-light.ttf -o .\msyhl0.ttf D:\font-spoofing\Original\ttc0\msyhl.ttx
+ttx -m D:\font-spoofing\input\sarasa-mono-sc-light.ttf -o .\msyhl1.ttf D:\font-spoofing\Original\ttc1\msyhl.ttx
+ttx -m D:\font-spoofing\input\FZPingXYSK.TTF -o .\simsun0.ttf D:\font-spoofing\Original\ttc0\simsun.ttx
+ttx -m D:\font-spoofing\input\FZPingXYSK.TTF -o .\simsun1.ttf D:\font-spoofing\Original\ttc1\simsun.ttx
+cd D:\font-spoofing\ttc
+python D:\font-spoofing\otf2otc.py -o msyh.ttc D:\font-spoofing\output\msyh0.ttf D:\font-spoofing\output\msyh1.ttf
+python D:\font-spoofing\otf2otc.py -o msyhbd.ttc D:\font-spoofing\output\msyhbd0.ttf D:\font-spoofing\output\msyhbd1.ttf
+python D:\font-spoofing\otf2otc.py -o msyhl.ttc D:\font-spoofing\output\msyhl0.ttf D:\font-spoofing\output\msyhl1.ttf
